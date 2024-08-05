d99ff588fab1033040d85d2e52d2a9b1c84f437c
andikatp <triprasetya_andika@yahoo.com>
2024-08-05 16:17:44 +0700

    adding router

--- fe/graphql_learn/lib/main.dart
+++ fe/graphql_learn/lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:graphql_learn/presentation/app_widget.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData.dark(),
-      home: const Scaffold(),
-    );
-  }
+  runApp(const ProviderScope(child: AppWidget()));
 }
---
+++ fe/graphql_learn/lib/presentation/home/screens/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Go To Next Page'),
+            ElevatedButton(
+              onPressed: () => context.pushNamed(Routes.location),
+              child: const Text('Next Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
