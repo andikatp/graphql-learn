acfe007a27079c433331ecc16b6bf1aa0fe700d0
andikatp <triprasetya_andika@yahoo.com>
2024-08-05 14:42:48 +0700

    adding user enitity

---
+++ fe/graphql_learn/lib/domain/users/entities/user.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class UserEntity with _$UserEntity {
+  const factory UserEntity({
+    required String id,
+    required String name,
+    required String username,
+  }) = _UserEntity;
+  const UserEntity._();
+
+  factory UserEntity.empty() => const UserEntity(id: '', name: '', username: '');
+
+  factory UserEntity.fromJson(Map<String, dynamic> json) => _$UserEntityFromJson(json);
+}
